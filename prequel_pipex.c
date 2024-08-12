222d52c61637068452475a3d595cf22f23c7a2a0
Nanelan <151051891+Nanelan@users.noreply.github.com>
2024-08-12 18:46:42 +0200

    Add files via upload

--- prequel_pipex.c
+++ prequel_pipex.c
@@ -6,7 +6,7 @@
 /*   By: crmunoz- <crmunoz-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 15:48:20 by crmunoz-          #+#    #+#             */
-/*   Updated: 2024/08/12 18:31:36 by crmunoz-         ###   ########.fr       */
+/*   Updated: 2024/08/12 18:46:02 by crmunoz-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -125,48 +125,3 @@ void	free_args(char	**args)
 		i++;
 	}
 }
-/*
-char	*remove_awk(char **argv)
-{
-	size_t	len;
-	char	*result;
-	char	*str;
-
-	str = ft_strdup(argv[2]);
-	len = ft_strlen(str);
-	result = malloc(len -2);
-	if (!result)
-		return (0);
-	str += 3;
-	while (*str != '\0')
-	{
-		if (*str != '\'' && *str != '\"')
-		{
-			result = str;
-			result++;
-		}
-		str++;
-	}
-	result = '\0';
-	printf("%s", result);
-	return (result);
-}
-
-char	**split_awk(char **argv)
-{
-	char	**awk;
-	int		len;
-
-	awk = malloc(sizeof(char *) * 3);
-	if (!awk)
-		return (NULL);
-	awk[2] = NULL;
-	awk[0] = ft_strdup("awk\0");
-	if (!awk[0])
-		return (NULL);
-	len = ft_strlen(remove_awk(argv));
-	awk[1] = malloc(sizeof(char *) * len);
-	awk[1] = remove_awk(argv);
-	return (awk);
-}
-*/
